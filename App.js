a00d69a4aca565d60a150c1a51929b569a927550
MarcelDev <marceldev96@gmail.com>
2023-11-25 20:14:24 +0100

    branch 2

--- App.js
+++ App.js
@@ -10,7 +10,7 @@ const App = () => {
 	    <Text>Yvana plombier 2</Text>
         <Text>Le Plombier</Text>
         <Text>Le Plombier uno</Text>
-        <Text>j ajoute la nouvelle branche</Text>
+        <Text>j ajoute la nouvelle branche test</Text>
         </View>
     );
 }
